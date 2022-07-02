a4f7b1c3871ed6f6e54514b803dc5fc518e1ea77
differential_gear <gingger@nonexistedmail.com>
2022-07-03 00:04:32 +0300

    Some html refactiring

--- flask_app/flask_forms.py
+++ flask_app/flask_forms.py
@@ -8,22 +8,31 @@ app = Flask(__name__)
 Bootstrap(app)
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route('/home')
+@app.route('/')
 def home():
-    if request.method == "POST":
-        start = request.form.get('first_element')
-        end = request.form.get('second_element')
+    return render_template('home.html')
+
+
+@app.route("/~connected", methods=['POST'])
+def search_connection():
+    start = request.form.get('first_element')
+    end = request.form.get('second_element')
+    path = None
+    if start and end:
         path = find_degrees(start, end, entries)
         print(start)
         print(end)
         print(path)
         if path:
-            arrowed_path = ' -> '.join(path)
+            arrowed_path = '<b> -> </b>'.join(path)
         else:
             arrowed_path = f'There is no connection between {start} and {end}'
-        return render_template('result.html', path=path, arrowed_path=arrowed_path, start=start, end=end)
-    return render_template('home.html')
+
+    return render_template('result.html', path=path, arrowed_path=arrowed_path, start=start, end=end)
 
 
 if __name__ == "__main__":
     app.run(debug=True, port=-1)
+
+
