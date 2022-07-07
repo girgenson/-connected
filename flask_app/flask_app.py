f530c07e93129770a6d7bb1d6983eb86737ba5ee
differential_gear <gingger@nonexistedmail.com>
2022-07-07 19:54:40 +0300

    It is possible to add category of entry

--- flask_app/flask_app.py
+++ flask_app/flask_app.py
@@ -10,7 +10,7 @@ from find_degrees import find_degrees
 from db import entries, type_of_entries
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectMultipleField, widgets
+from wtforms import StringField, SubmitField, SelectMultipleField, widgets, SelectField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 
@@ -30,49 +30,65 @@ db = SQLAlchemy(app)
 Bootstrap(app)
 moment = Moment(app)
 
+#
+# class Role(db.Model):
+#     __tablename__ = 'roles'
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(64), unique=True)
+#     users = db.relationship('User', backref='role', lazy='dynamic')
+#
+#     def __repr__(self):
+#         return f'<Role {self.name}>'
+#
+#
+# class User(db.Model):
+#     __tablename__ = 'users'
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(64), unique=True, index=True)
+#     role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+#
+#     def __repr__(self):
+#         return f'<User {self.username}>'
 
-class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role', lazy='dynamic')
 
-    def __repr__(self):
-        return f'<Role {self.name}>'
-
-
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-
-    def __repr__(self):
-        return f'<User {self.username}>'
+entry_category = db.Table('association', db.Model.metadata,
+                          db.Column('Entry_id', db.Integer, db.ForeignKey('Entry.id')),
+                          db.Column('Category_id', db.Integer, db.ForeignKey('Category.id'))
+                          )
 
 
 class Entry(db.Model):
-    __tablename__ = 'entry'
+    __tablename__ = 'Entry'
     id = db.Column(db.Integer, unique=True, index=True, primary_key=True)
     title = db.Column(db.String(255), unique=True, index=True)
     content = db.Column(db.String)
+    category = db.relationship('Category', secondary=entry_category)
 
     # editor == ...
 
     def __repr__(self):
-        return f'<Entry id: {self.id} title: {self.title}, content: {self.content}>'
+        return f'<Entry id: {self.id} title: {self.title}, content: {self.content}, ' \
+               f'categories: {", ".join([i.title for i in Entry.query.all()])}>'
+
+
+class Category(db.Model):
+    __tablename__ = 'Category'
+    id = db.Column(db.Integer, unique=True, index=True, primary_key=True)
+    title = db.Column(db.String(255), unique=True, index=True)
+
+    def __repr__(self):
+        return f'<Category id: {self.id} title: {self.title}>'
 
 
 # class Alias(db.Model):
 #     id = db.Column(db.Integer, unique=True, index=True, primary_key=True)
 #     title = db.Column(db.String(255), unique=True, index=True)
 #     entry_id = db.Column(db.Integer, db.ForeignKey('entries.id'))
-#
+
 
 # class TypesOfEntries(db.Model):
 #     id = db.Column(db.Integer, unique=True, index=True, primary_key=True)
 #     title = db.Column(db.String(255), unique=True, index=True)
-#
 
 
 class NameForm(FlaskForm):
@@ -83,10 +99,16 @@ class NameForm(FlaskForm):
 
 class AddEntryForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
-    choices = list(enumerate([i.capitalize() for i in type_of_entries.values()]))
+    # choices = list(enumerate([i.capitalize() for i in type_of_entries.values()]))
     # select_multiple_field = SelectMultipleField('Type', choices=[('', '')] + choices,
     #                                             option_widget=widgets.CheckboxInput())
     content = StringField('Content')
+    category = SelectMultipleField('Category', choices=[i.title for i in Category.query.all()], option_widget=widgets.CheckboxInput())
+    submit = SubmitField('Add')
+
+
+class AddCategoryForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
     submit = SubmitField('Add')
 
 
@@ -114,16 +136,38 @@ def add_entry():
     form = AddEntryForm()
     if form.validate_on_submit():
         title, content, alias = request.form.get('title'), request.form.get('content'), request.form.get('alias')
-        if Entry.query.filter_by(title=title).all():
-            flash('This title already exists')
-            form.title.data = ''
-            return render_template('add_entry.html', form=form)
+        # if Entry.query.filter_by(title=title).all():
+        #     flash('This title already exists')
+        #     form.title.data = ''
+        #     return render_template('add_entry.html', form=form)
         entry = Entry(title=title, content=content)
         db.session.add(entry)
         db.session.commit()
         session['entry_id'] = Entry.query.filter_by(title=title).all()[0].id
         return redirect(f'added_entry/{session.get("entry_id")}')
     return render_template('add_entry.html', form=form)
+
+
+@app.route('/add_category', methods=['GET', 'POST'])
+def add_category():
+    form = AddCategoryForm()
+    if form.validate_on_submit():
+        title = request.form.get('title')
+        if Category.query.filter_by(title=title).all():
+            flash('This category already exists')
+            form.title.data = ''
+            return render_template('add_category.html', form=form)
+        category = Category(title=title)
+        db.session.add(category)
+        db.session.commit()
+        return redirect('all_categories')
+    return render_template('add_category.html', form=form)
+
+
+@app.route('/all_categories')
+def all_categories():
+    _all_categories = Category.query.all()
+    return render_template('all_categories.html', _all_categories=_all_categories)
 
 
 @app.route('/added_entry/<entry_id>')
